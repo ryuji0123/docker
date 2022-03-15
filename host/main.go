22568027924093de4eeeae4313878ca6a7ad50ec
ryuji0123 <ryo1011.javelin@gmail.com>
2022-03-16 00:27:47 +0900

    enable host to receive json request including simple command information

--- host/main.go
+++ host/main.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"docker/host/opts"
+	"encoding/json"
 	"fmt"
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
 
+type CmdJsonBody struct {
+	Cmd string `json:"cmd"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Only POST request is acceptable\n")
+		return
+	}
+	if r.Header.Get("Content-type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Please set Content-type correctly\n")
+		return
+	}
+
+	byteArray, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var jsonBody CmdJsonBody
+	err = json.Unmarshal(byteArray, &jsonBody)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(w, jsonBody.Cmd+" command received\n")
 }
 
 func Log(h http.Handler) http.Handler {
@@ -29,7 +56,7 @@ func init() {
 
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("/", handler)
+	router.HandleFunc("/commands", handler)
 	logrus.Info("start server")
 	if err := http.ListenAndServe("localhost:"+opts.DefaultHTTPPort, Log(router)); err != nil {
 		logrus.WithField("event", "start server").Fatal(err)
